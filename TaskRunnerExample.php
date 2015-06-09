c39de4431d4a8805434622823304cd77f315616c
low_ghost <low_ghost@protonmail.com>
2015-06-09 06:56:11 -0400

    directory structure

---
+++ TaskRunnerExample.php
@@ -0,0 +1,18 @@
+<?php namespace low_ghost\PhpMultithread;
+
+require 'vendor/autoload.php';
+
+use low_ghost\PhpMultithread\Scheduler,
+    low_ghost\PhpMultithread\AsyncTask;
+
+$scheduler = new Scheduler;
+$asyncTask = new AsyncTask;
+
+$printResults = function($res){ return $res; };
+
+$scheduler->newTask($asyncTask->create('ls -a', $printResults));
+$scheduler->newTask($asyncTask->create('ls -l', $printResults));
+$scheduler->run();
+
+?>
+
